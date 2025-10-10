69e583615276fa0513a06a75444008dd979cc6dc
unknown <2DAW-ccaraba2502@IESMARTINEZM.ES>
2025-10-10 14:56:08 +0200

    Base del código User

---
+++ lib/funciones.php
@@ -0,0 +1,35 @@
+<?php
+
+function dump($var){
+    global $miVariable;
+    echo $miVariable;
+    echo '<pre>'.print_r($var,1).'</pre>';
+}
+
+function leerImput() {
+    
+}
+
+function getloginMarkup($loginData) {
+    
+}
+
+function leerArchivoCSV($rutaArchivoCSV) {
+    $tablero = [];
+
+    if (($puntero = fopen($rutaArchivoCSV, "r")) !== FALSE) {
+        while (($datosFila = fgetcsv($puntero)) !== FALSE) {
+            $tablero[] = $datosFila;
+        }
+        fclose($puntero);
+    }
+
+    return $tablero;
+}
+
+
+
+
+
+
+?>
