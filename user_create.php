e32341c5d98e3a1598778eda79f3709f143a5b88
unknown <2DAW-ccaraba2502@IESMARTINEZM.ES>
2025-10-17 14:48:24 +0200

    alguna corrección

--- user_create.php
+++ user_create.php
@@ -37,7 +37,7 @@ function writeCSV($preinfo) {
         fputcsv($out, $array);
     }
     
-    if(isset($_GET) && $_GET!=null) {
+    if(isset($_GET) && $_GET!=null && $_GET['user']!=null  && $_GET['email']!=null  && $_GET['rol']!=null ) {
         fputcsv($out, array($_GET['user'],$_GET['email'],$_GET['rol'],date('d-m-y')));
     }
     fclose($out);
@@ -79,25 +79,16 @@ function getLoginMarkup() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CREATE USER</title>
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+
 </head>
 <body>
+
   <?php echo $loginMarkup ?>
   <?php  writeCSV(leerArchivoCSV('data/users.csv'))?>
   <br><br><br><br>
   <a href="index.php">VOLVER ATRÁS</a>
     
-    <!-- <div class="divider">OR</div> -->
-    
-    <!-- <div class="social-login">
-        <div class="social-btn">G</div>
-        <div class="social-btn">F</div>
-        <div class="social-btn">X</div>
-    </div> -->
-    
-    <!-- <div class="footer">
-        Don't have an account? <a href="#">Sign up</a>
-    </div>
-  </div> -->
 
 </body>
 </html>
