0b2720791616b985185e353644f35c1a0f17d006
ChristianPort <ccaraba2502@g.educaand.es>
2025-10-21 23:41:17 +0200

    Info terminado

--- user_info.php
+++ user_info.php
@@ -1,22 +1,23 @@
 <?php
 
-function userMarkup($user) {
+function userMarkup($user_encoded) {
+    $user = unserialize(base64_decode($user_encoded));
     $output='';
 
     $output .='<div class="user">
-        <div class="image"><img src="" alt=""></div>
-        <div class="name"><p>Nombre: '.base64_decode(unserialize($user[0])).'</p></div>
-        <div class="email"><p>Email: '.base64_decode(unserialize($user[1])).'</p></div>
-        <div class="rol"><p>Rol '.base64_decode(unserialize($user[2])).'</p></div>
-        <div class="date"><p>Fecha de creación : '.base64_decode(unserialize($user[3])).'</p></div>
-    </div>';
+        <div class="image"><img src="src/img/user.png"></div>
+        <div class="name"><p>'.$user[0].'</p></div>
+        <div class="email"><p>'.$user[1].'</p></div>
+        <div class="rol"><p>Rol '.$user[2].'</p></div>
+        <div class="date"><p>Fecha de creación : '.$user[3].'</p></div>
+    </div>
+    <a href="user_index.php">VOLVER ATRÁS</a>';
 
     return $output;
 }
 
 function dump($var){
-    $des = base64_decode(unserialize($var));
-    echo '<pre>'.print_r($des,1).'</pre>';
+    echo '<pre>'.print_r($var,1).'</pre>';
 }
 
 ?>
@@ -28,12 +29,33 @@ function dump($var){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VER USER</title>
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <style>
+        .user{
+            margin: 5%;
+            height: 800px;
+            border: solid 4px grey;
+            box-shadow: grey;
+            border-radius:5%;
+        }
+        .name{
+            font-size:40px;
+        }
+        .image {
+            background-color:cyan;
+            border: solid 4px grey;
+            box-shadow: grey;
+            width: 50%;
+            margin:10%;
+            border-radius:5%;
+        }
+    </style>
 
 </head>
 <body>
 
-    <?php echo userMarkup($_GET['user']) ?>
-    <?php dump($_GET['user']) ?>;
+    <center>
+        <?php echo userMarkup($_GET['user']) ?>
+    </center>
     
 </body>
 </html>
