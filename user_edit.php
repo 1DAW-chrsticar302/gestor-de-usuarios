4bb842b0996437626951f9c395a10451e4c4c398
unknown <2DAW-ccaraba2502@IESMARTINEZM.ES>
2025-10-22 10:43:15 +0200

    Algo xd

---
+++ user_edit.php
@@ -0,0 +1,22 @@
+<?php
+
+    function dump($var){
+        echo '<pre>'.print_r($var,1).'</pre>';
+    }
+
+    function editUserMarkup($user) {
+        
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDIT USER</title>
+</head>
+<body>
+    
+</body>
+</html>
